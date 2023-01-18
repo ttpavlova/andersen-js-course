6c9f08a34eccb352396c58cb3e826ae5d3b21f71
Pavlova Tatyana <tpavlova.sm@yandex.ru>
2023-01-18 14:45:40 +0300

    Complete tasks 2-5

--- src/index.js
+++ src/index.js
@@ -19,10 +19,10 @@ import './styles/main.css';
  */
 
 import { task1Old, task1New } from './es-features/task1';
-// import { task2Old, task2New } from './es-features/task2';
-// import { task3Old, task3New } from './es-features/task3';
-// import { task4Old, task4New } from './es-features/task4';
-// import { task5Old, task5New } from './es-features/task5';
+import { task2Old, task2New } from './es-features/task2';
+import { task3Old, task3New } from './es-features/task3';
+import { task4Old, task4New } from './es-features/task4';
+import { task5Old, task5New } from './es-features/task5';
 // import { task6Old, task6New } from './es-features/task6';
 // import { task7Old, task7New } from './es-features/task7';
 // import { task8Old, task8New } from './es-features/task8';
@@ -37,25 +37,25 @@ import { task1Old, task1New } from './es-features/task1';
 console.log('task1 old', task1Old(10) === 500); // true
 console.log('task1 new', task1New(10) === 500); // true
 
-// console.log('=============');
+console.log('=============');
 
-// console.log('task2 old', task2Old(1, 2, 'test', false, 'new', 'best') === 12); // true
-// console.log('task2 new', task2New(1, 2, 'test', false, 'new', 'best') === 12); // true
+console.log('task2 old', task2Old(1, 2, 'test', false, 'new', 'best') === 12); // true
+console.log('task2 new', task2New(1, 2, 'test', false, 'new', 'best') === 12); // true
 
-// console.log('=============');
+console.log('=============');
 
-// console.log('task3 old', task3Old(['hello', 'test', 12])); // [ 1, 2, 'hello', 'test', 12 ]
-// console.log('task3 new', task3New(['hello', 'test', 12])); // [ 1, 2, 'hello', 'test', 12 ]
+console.log('task3 old', task3Old(['hello', 'test', 12])); // [ 1, 2, 'hello', 'test', 12 ]
+console.log('task3 new', task3New(['hello', 'test', 12])); // [ 1, 2, 'hello', 'test', 12 ]
 
-// console.log('=============');
+console.log('=============');
 
-// console.log('task4 old', task4Old()); // {x: 10, y: 10, bar: function, baztest: 'new field'}
-// console.log('task4 new', task4New()); // {x: 10, y: 10, bar: function, baztest: 'new field'}
+console.log('task4 old', task4Old()); // {x: 10, y: 10, bar: function, baztest: 'new field'}
+console.log('task4 new', task4New()); // {x: 10, y: 10, bar: function, baztest: 'new field'}
 
-// console.log('=============');
+console.log('=============');
 
-// console.log('task5 old', task5Old(['test', 42], { name: 'foo', val: 7 })); // ['test', 42, 'foo', 7]
-// console.log('task5 new', task5New(['test', 42], { name: 'foo', val: 7 })); // ['test', 42, 'foo', 7]
+console.log('task5 old', task5Old(['test', 42], { name: 'foo', val: 7 })); // ['test', 42, 'foo', 7]
+console.log('task5 new', task5New(['test', 42], { name: 'foo', val: 7 })); // ['test', 42, 'foo', 7]
 
 // console.log('=============');
 
